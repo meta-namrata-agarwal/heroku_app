62b8b61138f253ee04bb3002bf06eedce1b844ea
meta-namrata-agarwal <namrata.agarwal@metacube.com>
2021-06-01 17:22:09 +0530

    scaffold note

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :notes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -52,5 +54,7 @@ Rails.application.routes.draw do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
-  #   end
+
+  resources :notes
+  root 'notes#index'
 end
